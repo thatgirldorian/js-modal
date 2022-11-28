dd7679811f81979226c9099af1887a2a83b7b848
Debbie Otuagomah <thatgirldorian@gmail.com>
2022-11-28 09:05:48 +0400

    Added a way to close the modal when the overlay is clicked

--- app.js
+++ app.js
@@ -12,3 +12,13 @@ const modalCloseButton = document.querySelector('.jsModalClose')
 modalCloseButton.addEventListener('click', event => {
     document.body.classList.remove('modal-is-open')
 })
+
+//detect whether a user has clicked outside of the modal
+const modalOverlayClick = document.querySelector('.modal-overlay')
+
+modalOverlayClick.addEventListener('click', event => {
+   if (!event.target.closest('.modal')) {
+    //close modal
+    document.body.classList.remove('modal-is-open')
+   }
+})
